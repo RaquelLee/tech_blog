8a3511cae55e8a56d4e702271fbdd54581a8a79a
rsco <RaquelLee@users.noreply.github.com>
2021-04-09 16:58:14 -0400

    minor styling changes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,6 @@
 module.exports = {
   format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/
-    ${new Date(date).getDate()}/
-    ${new Date(date).getFullYear()} at 
-    ${new Date(date).getHours()}:
-    ${new Date(date).getMinutes()}`;
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} at 
+    ${new Date(date).getHours()}:${new Date(date).getMinutes()}`;
   }
 };
