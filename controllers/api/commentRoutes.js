7c2896fba9cc8106e8377ba9b2e9382f7eef2fe8
rsco <RaquelLee@users.noreply.github.com>
2021-04-09 17:15:09 -0400

    comment authorization added

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
 try {
     const newComment = await Comment.create({
     ...req.body,
